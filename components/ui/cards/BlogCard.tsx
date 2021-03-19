c701f7725f7cfc6933ca5f0199b78fdde0fd64df
ladell.erby <ladellerby@techsavagery.net>
2021-03-19 03:06:24 -0700

    - storybook init file

--- components/ui/cards/BlogCard.tsx
+++ components/ui/cards/BlogCard.tsx
@@ -1,8 +1,92 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Badge, Image, useColorModeValue, HStack } from "@chakra-ui/react";
 import React from "react";
+import { Blog } from "../../../interfaces";
+import { useRouter } from "next/router";
 
-const BlogCard = () => {
-  return <Box></Box>;
+type BlogCardProps = {
+  data: Blog;
+};
+
+const BlogCard = (props: BlogCardProps) => {
+  const router = useRouter();
+  const bg = useColorModeValue("white", "gray.900");
+  const color = useColorModeValue("gray.900", "white");
+  const {
+    title,
+    excerpt,
+    featuredImage,
+    date,
+    slug,
+    featuredImageAlt,
+    tags,
+  } = props.data;
+  return (
+    <Box
+      as="button"
+      maxW="sm"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      bg={bg}
+      color={color}
+      onClick={() => {
+        router.push(`/blog/${slug}`);
+      }}
+    >
+      <Image src={featuredImage} alt={featuredImageAlt} />
+
+      <Box bg={bg} color={color} p="6">
+        <Box d="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="teal">
+            Blog
+          </Badge>
+          <Box
+            color={color}
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+            bg={bg}
+          >
+            {title}
+          </Box>
+        </Box>
+        <Box
+          color={color}
+          fontWeight="semibold"
+          letterSpacing="wide"
+          fontSize="xs"
+          textTransform="uppercase"
+          ml="2"
+          bg={bg}
+        >
+          {date}
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+          textAlign="left"
+        >
+          {excerpt}
+        </Box>
+        <HStack></HStack>
+        <Box d="flex" mt="2" alignItems="center">
+          <HStack>
+            {tags.map((tag) => (
+              <Badge borderRadius="full" px="2" colorScheme="teal">
+                {tag}
+              </Badge>
+            ))}
+          </HStack>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default BlogCard;
---
+++ stories/Main.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SocialIcons from "../components/ui/widgets/SocialIcons";
+
+export default {
+  title: "Social Icons",
+  component: SocialIcons,
+};
+
+export const IconsDark = () => {
+  return <SocialIcons />;
+};
